c9f0a1fdac350e07e7d64308ebe102d7557c18ec
Fujipp Dev <anwker3008@gmail.com>
2025-09-22 10:18:04 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('HTTP listening on :3000');
 }
 bootstrap();
